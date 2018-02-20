40e7f2e1821eb8e4a78f685b6d2b48dac7da0eb1
Heather Beiyiyang <nord.heat@gmail.com>
2018-02-20 19:53:31 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://heather:heather@ds143778.mlab.com:43778/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
