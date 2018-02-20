cf7d2ebefad1b75ad49e1782b1ab33a3a306fb71
Heather Beiyiyang <nord.heat@gmail.com>
2018-02-20 20:34:24 +0000

    update dburl

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://heather:heather@ds143778.mlab.com:43778/yelpcamp");
+
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://heather:heather@ds143778.mlab.com:43778/yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
